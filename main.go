4b50157bd40d1f09075cf0e4eaa9687edb2b500a
Julien Ganichot <julien.ganichot@gmail.com>
2016-04-26 13:01:21 -0700

    Hotfix

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println(builder.NewQuery("user", "id").GetByID(3, nil))
 	fmt.Println(builder.NewQuery("user", "id").GetByID(42, []string{"col1", "col2", "col3"}))
 	fmt.Println(builder.NewQuery("user", "id").GetWhere(nil, nil, nil))
-	fmt.Println(builder.NewQuery("user", "id").GetWhere(nil, []string{"col1", "col2", "col3"}, []string{"bonjour", "bite", "4"}))
+	fmt.Println(builder.NewQuery("user", "id").GetWhere(nil, []string{"col1", "col2", "col3"}, []string{"bonjour", "bonsoir", "4"}))
 	fmt.Println(builder.NewQuery("user", "id").GetInnerJoin(nil, "col1", "col2", "col3"))
 	fmt.Println(builder.NewQuery("user", "id").GetInnerJoin([]string{"col1", "col2"}, "table2", "col1", "col2"))
 }
