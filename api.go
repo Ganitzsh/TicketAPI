5711b220b83eda64ebc5bfc611d09d61d6385e10
Julien Ganichot <julien.ganichot@gmail.com>
2016-05-01 11:53:36 -0700

    Added Ticket controller skeleton + Added ticket basic routes + Added gin-gonic router

--- api.go
+++ api.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"strconv"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/SQLApi/config"
+	"github.com/SQLApi/controllers"
 	"github.com/SQLApi/errors"
 	log "github.com/Sirupsen/logrus"
 )
@@ -19,6 +22,7 @@ type MySQLHandler struct {
 type API struct {
 	DBHandler *sql.DB
 	Settings  *config.APIConfig
+	Router    *gin.Engine
 }
 
 // NewAPI is the constructor of the API
@@ -52,7 +56,6 @@ func NewAPIFromFile(path string) (*API, error) {
 	default:
 		return nil, errors.NewUnsupportedDatabase("Unsuported database type " + settings.DBType)
 	}
-
 	if err != nil {
 		log.Error("Could not get a handle to the database")
 		return nil, err
@@ -60,13 +63,29 @@ func NewAPIFromFile(path string) (*API, error) {
 	api := &API{
 		DBHandler: db,
 		Settings:  settings,
+		Router:    gin.Default(),
 	}
+	api.configureRoutes()
 	if !api.available() {
 		return nil, errors.NewDBNotAvailable("Database not available")
 	}
 	return api, nil
 }
 
+func (api *API) configureRoutes() {
+	api.Router.GET("/tickets", controllers.Tickets)
+	api.Router.POST("/ticket/:id", controllers.NewTicket)
+	api.Router.DELETE("/ticket/:id", controllers.DeleteTicket)
+	api.Router.PATCH("/ticket/:id", controllers.EditTicket)
+}
+
+// Run starts listening on given port
+func (api *API) Run() {
+	port := ":" + strconv.Itoa(api.Settings.ListeningPort)
+	api.Router.Run(port)
+}
+
+//TODO: Add dbtype switch to check availability for multiple DB types
 func (api *API) available() bool {
 	err := api.DBHandler.Ping()
 	if err != nil {
---
+++ controllers/Ticket.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+// NewTicket creates a new ticket in the database
+func NewTicket(c *gin.Context) {
+
+}
+
+// DeleteTicket delete a ticket from the databse
+func DeleteTicket(c *gin.Context) {
+
+}
+
+// EditTicket edit a ticket in the database
+func EditTicket(c *gin.Context) {
+
+}
+
+// Tickets returns all the tickets from the database
+func Tickets(c *gin.Context) {
+
+}
